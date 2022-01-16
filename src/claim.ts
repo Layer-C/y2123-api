a12c083bffa7e5766764116aa44c947a34341e4d
shankar.nathan <shankar.nathan@grabtaxi.com>
2022-01-16 22:59:27 +0800

    save

--- src/claim.ts
+++ src/claim.ts
@@ -1,28 +1,69 @@
-import { APIGatewayProxyHandlerV2 } from 'aws-lambda';
-import { ethers } from 'ethers';
-import ABI from '../contract/Y2123.json';
-import apiResponses from './common/apiResponses';
-import AWS from 'aws-sdk';
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { ethers, utils } from "ethers";
+import ABI from "../contract/Y2123.json";
+import apiResponses from "./common/apiResponses";
+import AWS from "aws-sdk";
+import { generateEip712Hash } from "./common/eip712signature";
+const getClaimSecrets = require('./common/discordSecret.js').getClaimSecrets;
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
-  const provider = new ethers.providers.JsonRpcProvider(
-    process.env.ALCHEMY_API
-  );
-  const contract = new ethers.Contract(
-    process.env.Y2123_CONTRACT!,
-    ABI.abi,
-    provider
-  );
+  const provider = new ethers.providers.JsonRpcProvider(process.env.ALCHEMY_API);
+  const contract = new ethers.Contract(process.env.Y2123_CONTRACT!, ABI.abi, provider);
 
   const id = event.queryStringParameters?.id;
   if (!id) {
-    return apiResponses._400({ message: 'empty account id' });
+    return apiResponses._400({ message: "empty account id" });
   }
 
   const amount = 11;
   const accountNonce = 1;
 
-  return apiResponses._200({id});
+  let domain = [
+    { name: "name", type: "string" },
+    { name: "version", type: "string" },
+    { name: "chainId", type: "uint256" },
+    { name: "verifyingContract", type: "address" },
+  ];
+
+  //MyGoldz(uint256 amount,address account,uint256 nonce)
+  let claim = [
+    { name: "amount", type: "uint256" },
+    { name: "account", type: "address" },
+    { name: "nonce", type: "uint256" },
+  ];
+
+  let domainData = {
+    name: "y2123",
+    version: "1.0",
+    chainId: parseInt(process.env.CHAIN_ID!, 10),
+    verifyingContract: process.env.CLANS_CONTRACT!,
+  };
+
+  let claimData = {
+    amount: amount,
+    account: id,
+    nonce: accountNonce,
+  };
+
+  let eip712TypedData = {
+    types: {
+      EIP712Domain: domain,
+      MyGoldz: claim,
+    },
+    domain: domainData,
+    primaryType: "MyGoldz",
+    message: claimData,
+  };
+
+  const eip712TypedDataHashed = generateEip712Hash(eip712TypedData);
+  const claimSecrets = await getClaimSecrets();
+  const signingKey = new utils.SigningKey(claimSecrets.key3);
+
+  // Sign the message with the private key
+  const signature = signingKey.signDigest(eip712TypedDataHashed);
+  const joinSignature = utils.joinSignature(signature);
+
+  return apiResponses._200({ signature, joinSignature });
 };
--- src/common/discordSecret.js
+++ src/common/discordSecret.js
@@ -1,12 +1,14 @@
 const SecretsManager = require("aws-sdk").SecretsManager;
 
 const discordBotAPIKeyName = 'dev/y2123_bot';
+const claimKey = 'dev/claim';
 const secretsManager = new SecretsManager();
 
 /**
  * Cached Discord secrets so we can reduce warm start times.
  */
 let __discordSecrets = undefined;
+let __claimSecrets = undefined;
 
 /**
  * Gets the Discord secrets (public key, client ID, etc.) for use in our lambdas.
@@ -36,11 +38,21 @@ module.exports.getDiscordSecrets = async () => {
   }
   return __discordSecrets;
 };
-// TODO: remove later, unless we want to try getting the key via environment again
-// module.exports.getDiscordSecrets = async () => {
-//   console.log('found api keys? ' + (process.env.DISCORD_BOT_API_KEYS !== null));
-//   const keys = JSON.parse(process.env.DISCORD_BOT_API_KEYS);
-//   return keys;
-// }
 
 module.exports.apiKeyName = discordBotAPIKeyName;
+
+module.exports.getClaimSecrets = async () => {
+  if (!__claimSecrets) {
+    try {
+      const keys = await secretsManager.getSecretValue({
+        SecretId: claimKey,
+      }).promise();
+      if (keys.SecretString) {
+        __claimSecrets = JSON.parse(keys.SecretString);
+      }
+    } catch (exception) {
+      console.log(`Unable to get claim secrets: ${exception}`);
+    }
+  }
+  return __claimSecrets;
+};
