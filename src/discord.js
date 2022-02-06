0a698dfc02a7ff11fa1efe6502d39b75c718c1de
shankar.nathan <shankar.nathan@grabtaxi.com>
2022-02-06 18:56:05 +0800

    save

--- src/discord.js
+++ src/discord.js
@@ -1,28 +1,11 @@
 "use strict";
 
-const axios = require('axios');
-const {InteractionResponseType,InteractionType,verifyKey} = require('discord-interactions');
-const getDiscordSecrets = require('./common/secretsManager.js').getDiscordSecrets;
-const apiKeyName = require('./common/secretsManager.js').apiKeyName;
-const { SQS } = require('aws-sdk');
+const axios = require("axios");
+const { InteractionResponseType, InteractionType, verifyKey } = require("discord-interactions");
+const getDiscordSecrets = require("./common/secretsManager.js").getDiscordSecrets;
+const apiKeyName = require("./common/secretsManager.js").apiKeyName;
+const { SQS } = require("aws-sdk");
 const sqs = new SQS();
-
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Hello world. This my tech stack. There are many more like it, but this one is mine.",
-        input: event,
-      },
-      null,
-      2,
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
 
 module.exports.discordHandler = async (event, ctx, callback) => {
   // TODO: clean up later when confident about event structure
