4d3abe42ed62d815ac7b8bd67191041ba432e39a
Shanq <shanq@Shanqs-MacBook-Pro.local>
2022-04-05 18:09:19 +0800

    save

--- src/common/secretsManager.js
+++ src/common/secretsManager.js
@@ -1,37 +1,12 @@
 const SecretsManager = require("aws-sdk").SecretsManager;
 
-const discordBotAPIKeyName = "dev/y2123_bot";
 const claimKey = "dev/claim";
 const secretsManager = new SecretsManager();
 
 /**
- * Cached Discord secrets so we can reduce warm start times.
+ * Cached secrets so we can reduce warm start times.
  */
-let __discordSecrets = undefined;
 let __claimSecrets = undefined;
-
-/**
- * @returns The Discord secrets to be used.
- */
-module.exports.getDiscordSecrets = async () => {
-  if (!__discordSecrets) {
-    try {
-      const discordApiKeys = await secretsManager
-        .getSecretValue({
-          SecretId: discordBotAPIKeyName,
-        })
-        .promise();
-      if (discordApiKeys.SecretString) {
-        __discordSecrets = JSON.parse(discordApiKeys.SecretString);
-      }
-    } catch (exception) {
-      console.log(`Unable to get Discord secrets: ${exception}`);
-    }
-  }
-  return __discordSecrets;
-};
-
-module.exports.apiKeyName = discordBotAPIKeyName;
 
 module.exports.getClaimSecrets = async () => {
   if (!__claimSecrets) {
