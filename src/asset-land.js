791e2c8f4c43f2569b41b5965df85135a0f649b5
Shanq <shanq@Shanqs-MacBook-Pro.local>
2022-04-21 12:50:16 +0800

    save

--- src/asset-land.js
+++ src/asset-land.js
@@ -9,6 +9,7 @@ module.exports.handler = async (event) => {
     return apiResponses._400({ message: 'invalid id' });
   }
 
+  /*
   const params = {
     TableName: 'land-metadata',
     Key: {
@@ -18,4 +19,18 @@ module.exports.handler = async (event) => {
   const results = await dynamoDb.get(params).promise();
 
   return apiResponses._200(results.Item);
+  */
+
+  return apiResponses._200({
+    attributes: [
+      {
+        value: "Land Token",
+        trait_type: "Type"
+      }
+    ],
+    description: "[Y2123](https://www.y2123.com)",
+    name: `LAND#${id}`,
+    image: "https://img-land.y2123.io/0.png",
+    animation_url: "https://babylon1.s3.amazonaws.com/index.html?id=2"
+  });
 };
